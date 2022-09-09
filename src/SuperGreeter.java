c8fae5e29f8f42fec937be573224d832c1e35632
kjartanLeander <Kjar0321@stud.kea.dk>
2022-09-09 13:52:56 +0200

    Added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
 
+    Scanner scanner = new Scanner(System.in);
     public void start() {
         System.out.println("Det virker!");
-    }
 
-    public static void main(String[]args){
+    }
+    public void greet(){
+        System.out.println("Indtast dit navn");
+        String navn = scanner.nextLine();
+        System.out.println("Hej "+navn);
+
+    }
+    public static void main(String[]args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+        sg.greet();
+
     }
+
 
 }
